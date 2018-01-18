9bb1265f9aeff2f6e08b7f5826307e23178b2571
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-01-18 13:10:13 -0500

    made map class

---
+++ ZombieRush/src/FrontEnd.java
@@ -0,0 +1,26 @@
+import java.awt.event.MouseEvent;
+import javafx.animation.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public abstract class map {
+	private int mapheight;
+	private int mapwidth;
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
