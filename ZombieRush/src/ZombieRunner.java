9a65c83467ef5a4bbf57e4362b4f60a974fe3e25
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-01-18 12:51:36 -0500

    sdfsdfsdf

--- ZombieRush/src/ZombieRunner.java
+++ ZombieRush/src/ZombieRunner.java
@@ -1,8 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class ZombieRunner {
+public abstract class ZombieRunner extends Application{
 
-	public ZombieRunner() {
-		//BLAM
+	public static void ZombieRunner(String[] args) throws IOException {
+		
+		StringBuilder sb=new StringBuilder();
+		System.out.println("High Score List");  	
+    	String csvFile ="highscore.csv";
+    	BufferedReader br = null;
+    	br = new BufferedReader(new FileReader(csvFile));
+    	String line="";
+    	
+    	while ((line = br.readLine()) != null)
+    	{
+
+            // use comma as separator
+    		 String[] board = line.split(",");
+             System.out.println(board[0]+ "  "+board[1]);
+            sb.append(board[0]+ ","+board[1]+"\n");
+        }
+    	
+    	PrintWriter pw=new PrintWriter(new File("results.csv"));
+    	launch(args);
 	}
-
+	public void start(Stage primaryStage)
+	{
+		
+	}
 }
