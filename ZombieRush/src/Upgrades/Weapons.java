b99b8e841f33162afb22fbd3bb3fa8a85b2db8af
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-01-19 12:55:05 -0500

    Upgrades

---
+++ ZombieRush/src/Upgrades/Weapons.java
@@ -0,0 +1,14 @@
+package Upgrades;
+
+public class Weapons {
+	private int dmg;
+	private int price;
+	private boolean equipped;
+	
+	public Weapons(int dmg, int price, boolean equipped)
+	{
+		this.dmg = dmg;
+		this.price = price;
+		this.equipped = equipped;
+	}
+}
