188b25394d369ed84b3ecebd5b1414e97b9171a4
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-01-19 13:12:02 -0500

    dg

--- ZombieRush/src/ZombieRunner.java
+++ ZombieRush/src/ZombieRunner.java
@@ -5,11 +5,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-public abstract class ZombieRunner extends Application{
-
-	public static void ZombieRunner(String[] args) throws IOException {
-		//
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+ 
+public class ZombieRunner extends Application {
+    public static void main(String[] args) {
+    	
 		StringBuilder sb=new StringBuilder();
 		System.out.println("High Score List");  	
     	String csvFile ="highscore.csv";
@@ -26,11 +34,19 @@ public abstract class ZombieRunner extends Application{
             sb.append(board[0]+ ","+board[1]+"\n");
         }
     	
-    	PrintWriter pw=new PrintWriter(new File("results.csv"));
-    	launch(args);
-	}
-	public void start(Stage primaryStage)
-	{
-		
-	}
+    	PrintWriter pw=new PrintWriter(new File("highscore.csv"));
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+    	Text t = new Text(10, 50, "This is a test");
+    	t.setFont(new Font(20));
+
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(t);
+        primaryStage.setScene(new Scene(root, 300,300));
+        primaryStage.show();
+    }
 }
