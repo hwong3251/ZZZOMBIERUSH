9f68321042468d79e548e38872570ef9af68ae6f
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-01-30 12:45:32 -0500

    set up front page

--- ZombieRush/src/ZombieRunner.java
+++ ZombieRush/src/ZombieRunner.java
@@ -11,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,7 +19,7 @@ import javafx.stage.Stage;
 public class ZombieRunner extends Application {
     public static void main(String[] args) {
     	
-		StringBuilder sb=new StringBuilder();
+		/*StringBuilder sb=new StringBuilder();
 		System.out.println("High Score List");  	
     	String csvFile ="highscore.csv";
     	BufferedReader br = null;
@@ -33,20 +34,38 @@ public class ZombieRunner extends Application {
              System.out.println(board[0]+ "  "+board[1]);
             sb.append(board[0]+ ","+board[1]+"\n");
         }
-    	
-    	PrintWriter pw=new PrintWriter(new File("highscore.csv"));
+    	*/
+    	//PrintWriter pw=new PrintWriter(new File("highscore.csv"));
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
-    	Text t = new Text(10, 50, "This is a test");
-    	t.setFont(new Font(20));
+    	StackPane root = new StackPane();
+    	
+    	Text t = new Text("PLAY");
+    	t.setTranslateY(0);
+    	t.setFont(Font.loadFont("file:WarWound.otf",40));
+    	t.setFill(Color.RED);
+    	root.getChildren().add(t);
+    	
+    	Text t1 = new Text("SAVE");
+    	t1.setFont(Font.loadFont("file:WarWound.otf",40));
+    	
+    	t1.setTranslateY(50);
+    	root.getChildren().add(t1); 
+    	
+    	Text t2 = new Text("LOAD");
+    	t2.setFont(Font.loadFont("file:WarWound.otf",40));
+    	t2.setTranslateY(100);
+    	root.getChildren().add(t2); 
 
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(t);
-        primaryStage.setScene(new Scene(root, 300,300));
+    	Text t3 = new Text("HIGHSCORES");
+    	t3.setFont(Font.loadFont("file:WarWound.otf",40));
+    	t3.setTranslateY(150);
+    	root.getChildren().add(t3); 
+    	
+        primaryStage.setScene(new Scene(root, 500,500));
         primaryStage.show();
     }
 }
